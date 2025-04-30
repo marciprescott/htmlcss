63a56dd72be412b1af1978581d9d6f1a2f8d8713
Marci Prescott <mjprescott@me.com>
2025-04-30 17:52:36 -0600

    added mfm

---
+++ DOM Challenge Starting Files 2/index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("h1")[0].style.color = "yellow";
+document.querySelector("li a").style.color = "pink";
+document.getElementsByTagName("button")[0].style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
