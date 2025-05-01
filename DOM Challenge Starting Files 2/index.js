169c2c013bbb209248a134e0cb0a1b2818ecf609
Marci Prescott <mjprescott@me.com>
2025-04-30 18:16:02 -0600

    added dicee challenge

--- DOM Challenge Starting Files 2/index.js
+++ DOM Challenge Starting Files 2/index.js
@@ -2,3 +2,6 @@ document.getElementsByTagName("h1")[0].style.color = "yellow";
 document.querySelector("li a").style.color = "pink";
 document.getElementsByTagName("button")[0].style.backgroundColor = "yellow";
 document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
+document.querySelector("a");
+console.log(tag);
