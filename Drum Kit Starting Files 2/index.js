98a4d9bbb8dbb2783e1cd3cbdf35451071950d07
Marci Prescott <mjprescott@me.com>
2025-05-02 18:11:32 -0600

    updated drumkit challenge

--- Drum Kit Starting Files 2/index.js
+++ Drum Kit Starting Files 2/index.js
@@ -1,14 +1,18 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
+
     makeSound(buttonInnerHTML);
 
-    document.addEventListener("keydown", function(event)
-{
+    buttonAnimation(buttonInnerHTML);
+  });
+  document.addEventListener("keydown", function (event) {
     makeSound(event.key);
-})
-function makeSound(key) {
+    buttonAnimation(buttonInnerHTML);
+  });
+  function makeSound(key) {
     switch (key) {
       case "w":
         var audio = new Audio("sounds/crash.mp3");
@@ -41,6 +45,12 @@ function makeSound(key) {
       default:
         console.log(key);
     }
-  });
+  }
 }
-
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
