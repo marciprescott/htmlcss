430b432b0a68ab68db33aef6052561abef841f3a
Marci Prescott <mjprescott@me.com>
2025-05-04 16:27:55 -0600

    game files added

--- jquery/index.js
+++ jquery/index.js
@@ -7,3 +7,12 @@ $("input").keydown(function (event) {
 $("h1").on("mouseover", function () {
   $("h1").css("color", "red");
 });
+// Creating html elements
+$("h1").before("<button>NEW</button>");
+
+// $("button").on("click", function () {
+//   $("h1").fadeToggle();
+// });
+$("button").on("click", function () {
+  $("h1").animate({ opacity: 0.5 });
+});
