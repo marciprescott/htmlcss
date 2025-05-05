f316daf5085ab5ddfb47b29fe7dee630db907bb3
Marci Prescott <mjprescott@me.com>
2025-05-05 16:31:29 -0600

    game files uodated

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,14 +1,22 @@
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
-var randomChosenColor = buttonColors[randomNumber];
-gamePattern.push(randomChosenColor);
+
 // Use jQuery to select the button with the same id as the randomChosenColour
 // animate a flash to the button selected in step 1.
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-function nextSequence(); {
-
-   var randomNumber =  Math.floor(Math.random() * 3 ) + 1
-
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 3) + 1;
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  const audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
-$()
+$("button").on("click", function () {
+  var userChosenColor = this.attr("id");
+  console.log("user clicked:", userChosenColor);
+});
