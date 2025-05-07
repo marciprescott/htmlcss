532d1c8b4a003e105ee2e908a49204784698deba
Marci Prescott <mjprescott@me.com>
2025-05-07 13:45:53 -0600

    game files uodated

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -5,6 +5,8 @@ var level = 0;
 var started = false;
 $(document).keydown(function () {
   if (!started) {
+    $("#level-title").text("Level " + level);
+
     nextSequence();
     started = true;
   }
@@ -14,11 +16,36 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColor);
 
   playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
 });
 // Use jQuery to select the button with the same id as the randomChosenColour
 // animate a flash to the button selected in step 1.
-
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+  }
+  if (userClickedPattern.length === gamePattern.length) {
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+  } else {
+    playWrongSound;
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+  }
+}
+function playWrongSound() {
+  var wrongAudio = new Audio("sounds/wrong.mp3");
+  wrongAudio.play();
+}
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 3) + 1;
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
